45726464eb0ac56faf0a24033817195cd69c62b7
Deji Akala <deji.akala@gmail.com>
2015-02-24 16:12:16 +0000

    Remove autocomplete service

--- src/Controller/CountryAutocompleteController.php
+++ src/Controller/CountryAutocompleteController.php
@@ -9,41 +9,14 @@ namespace Drupal\country\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\country\CountryAutocomplete;
+use Drupal\Component\Utility\Unicode;
+use Drupal;
 
 
 /**
  * Returns autocomplete responses for countries.
  */
-class CountryAutocompleteController implements ContainerInjectionInterface {
-
-  /**
-   * The user autocomplete helper class to find matching user names.
-   *
-   * @var \Drupal\country\CountryAutocomplete
-   */
-  protected $countryAutocomplete;
-
-  /**
-   * Constructs a CountryAutocompleteController object.
-   *
-   * @param \Drupal\country\CountryAutocomplete $country_autocomplete
-   *   The country autocomplete helper class to find matching country names.
-   */
-  public function __construct(CountryAutocomplete $country_autocomplete) {
-    $this->countryAutocomplete = $country_autocomplete;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('country.autocomplete')
-    );
-  }
+class CountryAutocompleteController {
 
   /**
    * Returns response for the country name autocompletion.
@@ -53,13 +26,19 @@ class CountryAutocompleteController implements ContainerInjectionInterface {
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   A JSON response containing the autocomplete suggestions for countries.
-   *
-   * @see getMatches()
    */
   public function autocomplete(Request $request) {
-    $matches = $this->countryAutocomplete->getMatches($request->query->get('q'));
+    $matches = array();
+    $string = $this->countryAutocomplete->getMatches($request->query->get('q'));
+    if ($string) {
+      $countries = \Drupal::service('country_manager')->getList();
+      foreach ($countries as $iso2 => $country) {
+        if (strpos(Unicode::strtolower($country), Unicode::strtolower($string)) !== FALSE) {
+          $matches[] = array('value' => $country, 'label' => $country);
+        }
+      }
+    }
     return new JsonResponse($matches);
   }
-
 
 }
